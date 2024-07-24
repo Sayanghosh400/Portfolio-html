f9270c67395b6c35831b13dca98d9e053f55daf6
Sayanghosh400 <sayangithub400@gmail.com>
2024-07-24 12:27:24 +0530

    Initial

---
+++ Own Website/j.js
@@ -0,0 +1,37 @@
+let d = document.getElementById("mistidiv")
+          let b = document.getElementById("rsglla")
+          b.addEventListener("click",txt)
+
+        function txt(e) {
+          let c = document.createElement('p')
+          
+          if (c.class=="rasu") {
+            let 
+          } else {
+            c.className = "rasu"
+            c.textContent="Rasgulla, also known in Rasagola, Rosogola or Rosogolla, is a syrupy dessert popular in the Indian subcontinent and regions with South Asian diaspora. It is made from ball-shaped dumplings of chhena (an Indian cottage cheese) and semolina dough, cooked in light sugar syrup made of sugar. This is done until the syrup permeates the dumplings."
+
+            d.appendChild(c)
+        }
+          
+        }
+        
+        let gu = document.getElementById("glbjmn")
+        gu.addEventListener("click",text)
+
+      function text(e) {
+        let gul = document.createElement('p')
+        gul.textContent="Gulab jamun (also spelled gulaab jamun) lit. 'Rose water berry' or 'Rose berry' is a sweet confectionary or dessert, originating in the Indian subcontinent and a type of mithai popular in India, Pakistan, Nepal, the Maldives (where it is known as gulab ki janu), and Bangladesh, as well as Myanmar. It is the national dessert of Pakistan. It is also common in nations with substantial populations of people with South Asian heritage, such as Mauritius, Fiji, Gulf states, the Malay Peninsula, Great Britain, South Africa, and the Caribbean countries of Jamaica, Trinidad and Tobago, Guyana, and Suriname. It is made mainly from milk solids, traditionally from khoya, which is milk reduced to the consistency of a soft dough. Modern recipes call for dried or powdered milk instead of khoya. It is often garnished with dried nuts such as almonds and cashews to enhance flavour."
+
+        d.appendChild(gul)
+      }
+      
+        let ja = document.getElementById("jlb")
+        ja.addEventListener("click",teext)
+
+      function teext(e) {
+        let jal = document.createElement('p')
+        jal.textContent="Jalebi is a popular sweet snack in south and west Asia, Africa, and Mauritius. It goes by many names, including jilapi, jilebi, jilipi, zulbia, jerry, mushabak, z’labia, or zalabia.Jalebi can be served warm or cold. They have a somewhat chewy texture with a crystallized sugary exterior coating. Citric acid, lime juice and rose water is sometimes added to the syrup. Similar but distinct dishes include imarti, chhena jalebi, lokma, zalabiyeh, and bamiyeh. To keep it crisp and to prevent it from getting soggy, commercially made Jalebi has Rangkat (which is Sodium dithionite and also called Hydro) added to the Jalebi batter recipe."
+        
+        d.appendChild(jal)
+      }
